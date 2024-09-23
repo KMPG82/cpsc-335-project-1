9c6f23c56e08c8565cc49380bb5349c275bd272d
Kevin <kevinponting82@gmail.com>
2024-09-23 15:40:53 -0700

    added comments for both algos

--- project-1-algo-1.py
+++ project-1-algo-1.py
@@ -1,32 +1,28 @@
 def minSwapsCouples(row):
     swaps=0
     i=0
-    while(i<len(row)):
-        if row[i]%2==0:
-            #even nums will have a number +1 greater than them
-            operation='row[i]+1'   
-        else:
-            #odd numbers will have a number -1 less than them
-            operation='row[i]-1'
+    while(i<len(row)): #iterate for each 2n number in the list
+        if row[i]%2==0: #check if number is even
+            operation='row[i]+1' #even numbers will be paired with a number +1 greater than them   
+        else: #if number is not even, then it is odd
+            operation='row[i]-1' #odd numbers will be paired with a number -1 less than them
 
-        if row[i+1]!=eval(operation):
-            j=i+2
-            while(j<len(row)):
-                if row[j]==eval(operation):
-                    row[i+1], row[j] = row[j], row[i+1]
-                    swaps+=1
-                    break
-                j+=1
-                
-        i+=2
+        if row[i+1]!=eval(operation): #check if current number is already next to its corresponding pair
+            j=i+2 #start from i+2 index of the list
+            while(j<len(row)): #iterate through list to find corresponding pair
+                if row[j]==eval(operation): #check if number at index j is the corresponding pair for number at index i
+                    row[i+1], row[j] = row[j], row[i+1] #if it is the corresponding pair, swap with the number at index i+1
+                    swaps+=1 #add one to swaps
+                    break #found pair so end this loop
+                j+=1 #iterate to next element if the number at index j was not the corresponding pair        
+
+        i+=2 #iterate by 2
     
-    return swaps
+    return swaps #return minimum amount of swaps it took to pair each 2n number with their corresponding pair
 
+#test cases
 row = [0,2,1,3]
-test=minSwapsCouples(row)
-print(test)
+print(minSwapsCouples(row))
 
 row = [3,2,0,1]
-test=minSwapsCouples(row)
-print(test)
-
+print(minSwapsCouples(row))
--- project-1-algo-2.py
+++ project-1-algo-2.py
@@ -1,61 +1,57 @@
 def canCompleteCircuit(gas, cost, mpg):
     i=0
-    #iterate through each city as a starting city
-    while i<len(cost):
-        if (cost[i]/mpg) <= gas[i]:
-            totalGas=gas[i] #refill with gas from starting city
-            previousCity=i #keep track of previous city
+    while i<len(cost): #iterate through each city in list as the city we start from
+        if (cost[i]/mpg) <= gas[i]: #check if there is enough gas at starting to reach next city
+            totalGas=gas[i] #fill with gas from starting city
+            totalGas-=(cost[i]/mpg) #subtract amount of gas it takes to reach next city
             j=(i+1)%len(cost) #move to next city
+            
             condition=True
-
-            while condition:
-                totalGas-=(cost[previousCity]/mpg) #subtract current gas from cost of travel
-
-                if totalGas<0: #if total gas is now negative, i cannot be the starting city because did not reach next city
-                    break
-                else: #total gas is no less than 0, meaning successfully reach next city
-                    if j==i:
+            while condition: #loop for traversing cities starting from city i
+                if totalGas<0: #if current toal gas is negative, i cannot be the starting city because did not complete a full circuit
+                    break #end loop for traversing cities starting from city i
+                else: #total gas is no less than 0, meaning successfully reached next city
+                    if j==i: #if city j is the same as city i we completed the circuit
                         condition=False
-                        return i
-                    else:
-                        totalGas+=gas[j] #refill with gas at current city
-                        previousCity=j
+                        return i #return the index of the city we started at bc it is the valid starting city for this instance
+                    else: #if we have not completed circuit yet, then continue traversal
+                        totalGas=totalGas+gas[j]-(cost[j]/mpg) #refill with gas at current city and subtract gas it would take to reach next city
                         j=(j+1)%len(cost) #go to next city
                         
-        i+=1
+        i+=1 #iterate to next potential starting city
 
+#test cases
 city_distances = [5, 25, 15, 10, 15]
 fuel = [1, 2, 1, 0, 3 ]
 mpg = 10
-test=canCompleteCircuit(fuel, city_distances, mpg)
-print(test)
+print(canCompleteCircuit(fuel, city_distances, mpg))
 
 city_distances = [3,4,5,1,2]
 fuel = [1,2,3,4,5]
 mpg = 1
-test=canCompleteCircuit(fuel, city_distances, mpg)
-print(test)
+print(canCompleteCircuit(fuel, city_distances, mpg))
 
 city_distances = [3,4,3] 
 fuel = [5,3,4]
 mpg = 1
-test=canCompleteCircuit(fuel, city_distances, mpg)
-print(test)
-
-city_distances = [10, 20, 30, 40]
-fuel = [1, 2, 3, 4]
-mpg = 10
-test=canCompleteCircuit(fuel, city_distances, mpg)
-print(test)
-
-city_distances = [5, 25, 15, 10, 15]
-fuel = [1, 2, 1, 0, 3]
-mpg = 10
-test=canCompleteCircuit(fuel, city_distances, mpg)
-print(test)
+print(canCompleteCircuit(fuel, city_distances, mpg))
 
 city_distances = [10, 20, 30, 10]
 fuel = [1, 0, 2, 5]
 mpg = 10
-test=canCompleteCircuit(fuel, city_distances, mpg)
-print(test)
+print(canCompleteCircuit(fuel, city_distances, mpg))
+
+city_distances = [5, 10, 15, 5]
+fuel = [1, 2, 1, 3]
+mpg = 5
+print(canCompleteCircuit(fuel, city_distances, mpg))
+
+city_distances = [8, 12, 6, 10]
+fuel = [2, 0, 3, 5]
+mpg = 4
+print(canCompleteCircuit(fuel, city_distances, mpg))
+
+city_distances = [3,4,5,1,2]
+fuel = [1,2,3,4,5]
+mpg = 1
+print(canCompleteCircuit(fuel, city_distances, mpg))
