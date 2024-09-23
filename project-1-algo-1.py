e329e6808ffc5b2734c896e19588157df7caa406
Kevin <kevinponting82@gmail.com>
2024-09-22 20:21:46 -0700

    algo 1, slower execution time but less lines of code

--- project-1-algo-1.py
+++ project-1-algo-1.py
@@ -4,30 +4,25 @@ def minSwapsCouples(row):
     while(i<len(row)):
         if row[i]%2==0:
             #even nums will have a number +1 greater than them
-            if row[i+1]!=row[i]+1:
-                j=i+2
-                while(j<len(row)):
-                    if row[j]==row[i]+1:
-                        row[i+1], row[j] = row[j], row[i+1]
-                        swaps+=1
-                        break
-                    j+=1         
-        else:  
+            operation='row[i]+1'   
+        else:
             #odd numbers will have a number -1 less than them
-            if row[i+1]!=row[i]-1:
-                j=i+2
-                while(j<len(row)):
-                    if row[j]==row[i]-1:
-                        row[i+1], row[j] = row[j], row[i+1]
-                        swaps+=1
-                        break
-                    j+=1
+            operation='row[i]-1'
+
+        if row[i+1]!=eval(operation):
+            j=i+2
+            while(j<len(row)):
+                if row[j]==eval(operation):
+                    row[i+1], row[j] = row[j], row[i+1]
+                    swaps+=1
+                    break
+                j+=1
                 
         i+=2
     
     return swaps
 
-row = [0, 2, 1, 3]
+row = [0,2,1,3]
 test=minSwapsCouples(row)
 print(test)
 
@@ -35,11 +30,3 @@ row = [3,2,0,1]
 test=minSwapsCouples(row)
 print(test)
 
-# def loop(self, row, operation, start):
-#     j=start+2
-#     while(j<len(row)):
-#         if row[j]==eval(operation):
-#             row[start+1], row[j] = row[j], row[start+1]
-#             self.swaps+=1
-#             break
-#         j+=1
