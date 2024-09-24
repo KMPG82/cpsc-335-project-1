30d823c4c692b6e4e9d8b78862efc7974732499a
Kevin <kevinponting82@gmail.com>
2024-09-23 22:42:19 -0700

    cleaned up code

--- project-1-algo-1.py
+++ project-1-algo-1.py
@@ -1,7 +1,8 @@
 def minSwapsCouples(row):
     swaps=0
     i=0
-    while(i<len(row)): #iterate for each 2n number in the list
+    while i<len(row): #iterate for each 2n number in the list
+        operation='' #initialize operation
         if row[i]%2==0: #check if number is even
             operation='row[i]+1' #even numbers will be paired with a number +1 greater than them   
         else: #if number is not even, then it is odd
@@ -9,7 +10,7 @@ def minSwapsCouples(row):
 
         if row[i+1]!=eval(operation): #check if current number is already next to its corresponding pair
             j=i+2 #start from i+2 index of the list
-            while(j<len(row)): #iterate through list to find corresponding pair
+            while j<len(row): #iterate through list to find corresponding pair
                 if row[j]==eval(operation): #check if number at index j is the corresponding pair for number at index i
                     row[i+1], row[j] = row[j], row[i+1] #if it is the corresponding pair, swap with the number at index i+1
                     swaps+=1 #add one to swaps
